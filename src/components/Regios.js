dad5e8a069d66adacfbfed574a3a29ef419daa85
Michiel thuis <michiel_thuis@Michiels-MacBook-Pro.local>
2019-03-11 21:26:06 +0100

    Adjustment for segementation of circle groups

--- src/components/Regios.js
+++ src/components/Regios.js
@@ -4,38 +4,97 @@ const healthRegionData = require('../data/zorgkantoor_regios.json');
 
 const distinctRegios = [...new Set(healthRegionData.map(d => d.Zorgkantoorregio))];
 
+let regioData = [];
+distinctRegios.map((d, i) => {
+  regioData[i] = [];
+  return healthRegionData.reduce((previous, d) => {
+    if (previous === d.Zorgkantoorregio) {
+      regioData[i].push({Regio: d.Zorgkantoorregio, Gemeente: d.Gemeente});
+    }
+    return previous;
+  },d);
+});
+
+
+
+
 export class Regios extends Component {
 
   handleClick(e) {
-    console.log(distinctRegios);
+    console.log(regioData);
   };
-  componentDidMount({
-    console.log('CDM');
-    svg.selectAll('circle')
-      .data(data)
-      .enter()
-      .append("")
-      .attr("x", (d, i) => i * 70)
-      .attr("y", 0)
-      .attr("width", 25)
-      .attr("height", (d, i) => d)
-      .attr("fill", "green");
-  })
+
+  componentDidMount() {
+    const svg = d3.select('svg#svg');
+
+    const width = 1000, height = 800;
+    svg.attr('width', width)
+    .attr('height', height);
+
+    const myColor = d3.scaleOrdinal()
+    .domain(distinctRegios)
+    .range(d3.schemeCategory10);
+
+    const arrValue = 2000 / distinctRegios.length;
+    const categorienPosities = [];
+    distinctRegios.map((d, i) => {
+      categorienPosities.push({regio: d, value: i * arrValue});
+    });
+
+    console.log(categorienPosities);
+
+    const xCenter = healthRegionData.map((d,i) => {
+      categorienPosities.map((e, i) => {
+        if (e.regio === d.Zorgkantoorregio) {
+          d.pos = e.value;
+         }
+      });
+    })
+
+    console.log(healthRegionData);
+
+    const simulation = d3.forceSimulation(healthRegionData)
+    .force('charge', d3.forceManyBody().strength(5))
+    .force('x', d3.forceX().x(d => d.pos))
+    .force('y', d3.forceY().y(200))
+    // .force('center', d3.forceCenter(width / 2, height / 2))
+    .force('collision', d3.forceCollide().radius(d => d.Inwoneraantal/100000))
+    .on('tick', ticked);
+
+
+
+    // simulation.force('x', d3.forceX().x(function(d) {
+    //   return xCenter[d.Zorgkantoorregio];
+    // }));
+
+    function ticked() {
+      const u = d3.select('svg')
+      .selectAll('circle')
+      .data(healthRegionData)
+
+      u.enter()
+        .append('circle')
+        .attr('r', d => d.Inwoneraantal/100000)
+        .merge(u)
+        .attr('cx', d => d.x)
+        .attr('cy', d => d.y)
+        .style("fill", d => myColor(d.Zorgkantoorregio))
+        .attr('stroke-width', '2')
+
+      u.exit().remove()
+    }
+
+
+  };
+
+
 
   render() {
     console.log(healthRegionData);
+    console.log(regioData);
     // Make healthRegionData usefull -->  regioData
 
-    let regioData = [];
-    distinctRegios.map((d, i) => {
-      regioData[i] = [];
-      return healthRegionData.reduce((previous, d) => {
-        if (previous === d.Zorgkantoorregio) {
-           regioData[i].push({Regio: d.Zorgkantoorregio, Gemeente: d.Gemeente, Inwoneraantal: d.Inwoneraantal});
-        }
-        return previous;
-      },d);
-    });
+
 
     return (
       <div className="main">
